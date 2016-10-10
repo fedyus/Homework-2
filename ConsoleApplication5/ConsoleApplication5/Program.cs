5b32574070cfb23046b96c148435bfe917027724
admin <admin@HOME>
2016-10-10 21:09:16 +0300

    интеграл 1-4

---
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a=2, b=3, s=0, n,x,i;
+            n = double.Parse(Console.ReadLine());
+
+            x = a;
+            i= (b - a) / n;
+            while (x<=b)
+            {
+                s += -Math.Sin(Math.Tan(x+i/2));
+                x += i;
+            }
+            Console.WriteLine(s/n);
+            Console.ReadLine();
+
+        }
+    }
+}
