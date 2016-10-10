29ceb5f64f681b314cef39b3fda75655b02a4802
admin <admin@HOME>
2016-10-10 22:16:04 +0300

    интеграл 4-3

---
+++ ConsoleApplication6/ConsoleApplication6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a=0.5, b=2.5, s;
+
+            s =0.559* (b - a) * (-Math.Tan(1 / a + a) - 4 * Math.Tan(2 / (a + b) + (a + b) / 2) - Math.Tan(1 / b + b)) / 3;
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
