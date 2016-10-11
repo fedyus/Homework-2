19c07be5ecb62d0b75746a17a08f2f958ae0ea0b
admin <admin@HOME>
2016-10-11 19:32:15 +0300

    Сумма ряда 6

---
+++ ConsoleApplication8/ConsoleApplication8/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                int[] a = new int[50];
+            int i = 0,sum = 0,k = 0,j = 0;
+            Random z = new Random();
+            while (i < 50)
+            {
+                a[i] = z.Next(-100, 100);
+                i++;
+            }
+
+
+            i = 0;
+            while (i < 50)
+            {
+                if (a[i] > 0)
+                    sum += a[i];
+                i++;
+            }
+            Console.WriteLine(sum);
+
+            i = 0;
+            while (i < 49)
+            {
+                if ((a[i] >= 0 && a[i + 1] < 0) || (a[i] < 0 && a[i + 1] >= 0))
+                    k++;
+                i++;
+            }
+            Console.WriteLine(k);
+
+            k = 0;
+            i = 0;
+            while (i < 49)
+            {
+                if (a[i] >= a[i + 1])
+                    k++;
+                i++;
+            }
+            k = 0;
+            i = 0;
+            if (k == 49)
+                Console.WriteLine("Yes");
+            else
+            {
+                while (i < 49)
+                {
+                    if (a[i] <= a[i + 1])
+                        k++;
+                    i++;
+                }
+                if (k == 49)
+                    Console.WriteLine("Yes");
+                else
+                    Console.WriteLine("No");
+            }
+
+            k = 0;
+            i = 0;
+            while (i < 49)
+            {
+                j = i + 1;
+                while (j < 49)
+                {
+                    if (a[i] == a[j])
+                    {
+
+                        k++;
+                    }
+                    j++;
+
+                }
+                i++;
+            }
+
+
+            if (k > 1)
+                Console.WriteLine($"Yes, {50 - k} ");
+            else
+                Console.WriteLine("No");
+
+            Console.WriteLine(k);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApplication9/ConsoleApplication9/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            int[] a = new int[n];
+            int i = 1,j,f ;
+            int[] b = new int[n];
+            int[] c = new int[2*n];
+            Random z = new Random();
+            a[0] = 1;
+            b[0] = 1;
+            while (i < n)
+            {
+                a[i] = z.Next(a[i-1], i * 10);
+                b[i] = z.Next(b[i-1], i * 10);
+                i++;
+            }
+
+            i = 0;
+            while (i <  n )
+            {
+                Console.Write("{0,3}", a[i]);
+                i++;
+            }
+            Console.WriteLine();
+            i = 0;
+            while (i <  n )
+            {
+                Console.Write("{0,3}", b[i]);
+                i++;
+            }
+            Console.WriteLine();
+            i = 0;
+            j = 0;
+            f = 0;
+            while (i <= 2 * n - 1)
+            {
+                if ((j <= n - 1) && (f <= n - 1))
+                {
+                    if (a[j] > b[f])
+                    {
+                        c[i] = b[f];
+                        f++;
+                        i++;
+                    }
+                    else
+                    {
+                        if (a[j] < b[f])
+                        {
+                            c[i] = a[j];
+                            j++;
+                            i++;
+                        }
+                        else
+                        {
+                            c[i] = a[j];
+                            c[i+1] = b[f];
+                            j++;
+                            f++;
+                            i +=2;
+                        }
+                    }
+                }
+                else
+                {
+                    if (j == n)
+                    {
+                        c[i] = b[f];
+                        f++;
+                        i++;
+                    }
+                    else
+                    {
+                        c[i] = a[j];
+                        j++;
+                        i++;
+                    }
+                }
+
+            }
+            i = 0;
+            while (i<2*n)
+            {
+                Console.Write("{0,3}", c[i]);
+                i++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
