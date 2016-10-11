3b2a66b07d7cfe64fe5427db10b496442004702a
admin <admin@HOME>
2016-10-11 20:29:39 +0300

    Сумма ряда 13 (переделка)

---
+++ ConsoleApplication12/ConsoleApplication12/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, e, a1 = 1, a2 = 1, b, c, d, g, k = 1;
+            e = double.Parse(Console.ReadLine());
+            x = double.Parse(Console.ReadLine());
+
+            b = -1;
+            c = x;
+            d = 3;
+            g = 2;
+            a2 += b * c * d / g;
+            while (Math.Abs(a1 - a2) > e)
+            {
+                a1 += b * c * d / g;
+                k++;
+                b *= -1;
+                c *= x;
+                d *= (2 * k + 1);
+                g *= 2 * k;
+                a2 += b * c * d / g;
+            }
+
+            Console.WriteLine(a2);
+            Console.ReadLine();
+        }
+    }
+}
